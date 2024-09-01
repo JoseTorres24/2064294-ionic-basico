2995220159609710c80c8b8c2a7eb5247c117939
JoseTorres24 <luis.lopeztr@uanl.edu.mx>
2024-09-01 16:52:00 -0600

    agregando el css y algunos acomodos mas

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function iniciarSesion(){
 
     var Usuariovalor = Usuario.value;
     //parrafo.innerHTML = "Login Correcto para " + Usuariovalor;
-    parrafo.innerHTML = `Inicio de sesion con exito para ${Usuariovalor}`;
+    parrafo.innerHTML = `Inicio de sesion con exito para <strong> ${Usuariovalor}</strong>`;
 }
 function Registro(){
     var Usuariovalor = Usuario.value;
-    parrafo.innerHTML = `Registro con exito para ${Usuariovalor}`;
+    parrafo.innerHTML = `Registro con exito para <strong> ${Usuariovalor}</strong>`;
 }
