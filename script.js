a7a02c2e1f1fbed68f9a4d2ad2667390ffc79917
JoseTorres24 <luis.lopeztr@uanl.edu.mx>
2024-09-07 13:53:53 -0600

    Practica 4 terminanda con validaciones por si no se ingresa nada

--- script.js
+++ script.js
@@ -9,16 +9,20 @@ function imprimirCosastotal(){
     
     var articuloValue = articulo.value;
     var montoValue = monto.value;
-    total = total + parseFloat(montoValue);
+  
+    // Implemente algunas codiciones por si al caso no se ingresa nada en los inputs
+    if (montoValue !=='' && articuloValue!== ''){
+        total = total + parseFloat(montoValue);
    
-    var elemento = document.createElement("li");
-    elemento.innerHTML = `${articuloValue} ...........$${montoValue}`
-    lista.appendChild(elemento);
-
+        var elemento = document.createElement("li");
+        elemento.innerHTML = `${articuloValue} ...........$${montoValue}`
+        lista.appendChild(elemento);
+        parrafoTotal.innerHTML = `Total: <strong>${total}</strong>`
+        document.getElementById("Articulo").value ='';
+        document.getElementById("Monto").value =''
+    }
+   
     
-    parrafoTotal.innerHTML = `Total: <strong>${total}</strong>`
 
-    document.getElementById("Articulo").value ='';
-    document.getElementById("Monto").value =''
 }
 
