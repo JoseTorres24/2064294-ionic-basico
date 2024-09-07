198e84935a73fd8daac14440e0ad6a85eef197d5
mmalexei <125532998+JoseTorres24@users.noreply.github.com>
2024-09-07 10:34:29 -0600

    Practica 4 introduccion a ionic

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-var Usuario = document.getElementById("Usuario");
-var Contraseña = document.getElementById("Contraseña");
-var parrafo = document.querySelector("p");
 
-function iniciarSesion(){
+    var articulo = document.getElementById("Articulo");
+    var monto = document.getElementById("Monto");
+    var total = 0;
+    var lista = document.getElementById("lista");
+    var parrafoTotal = document.querySelector("p");
+function imprimirCosastotal(){
+    
+    var articuloValue = articulo.value;
+    var montoValue = monto.value;
+    total = total + parseFloat(montoValue);
+   
+    var elemento = document.createElement("li");
+    elemento.innerHTML = `${articuloValue} ...........$${montoValue}`
+    lista.appendChild(elemento);
 
-    var Usuariovalor = Usuario.value;
-    //parrafo.innerHTML = "Login Correcto para " + Usuariovalor;
-    parrafo.innerHTML = `Inicio de sesion con exito para <strong> ${Usuariovalor}</strong>`;
+    
+    parrafoTotal.innerHTML = `<strong>${total}</strong>`
+
+    articuloValue = '';
+    montoValue='';
 }
-function Registro(){
-    var Usuariovalor = Usuario.value;
-    parrafo.innerHTML = `Registro con exito para <strong> ${Usuariovalor}</strong>`;
-}
+
