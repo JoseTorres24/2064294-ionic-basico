c94ee4cf33950edd771c51654c10675e79a4bfd9
JoseTorres24 <luis.lopeztr@uanl.edu.mx>
2024-09-30 00:46:49 -0600

    Practica 7 parte 2

--- miApp-2064294/src/app/lista-alumnos/lista-alumnos.component.ts
+++ miApp-2064294/src/app/lista-alumnos/lista-alumnos.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IonList,IonItem } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-lista-alumnos',
   templateUrl: './lista-alumnos.component.html',
   styleUrls: ['./lista-alumnos.component.scss'],
-  standalone:true
+  standalone:true,
+  imports:[IonItem,IonList,CommonModule]
 })
 export class ListaAlumnosComponent  implements OnInit {
+
+
+   alumnos:any = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"];
 
   constructor() { }
 
   ngOnInit() {}
+  
+  
+   
+  
 
-  alumnos : any = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"];
+   
+
+   
 
 }
