22c5d59680cf0eafc70ca4e2846c3b87d4a6f22f
Satoriowo <valeriana4151@gmail.com>
2024-10-12 11:23:11 -0600

    Avances practica 10

--- miApp-2064294/src/app/app.routes.ts
+++ miApp-2064294/src/app/app.routes.ts
@@ -6,20 +6,20 @@ export const routes: Routes = [
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full',
-  },
+  
   {
     path: 'productos',
     component: ProductosComponent,
     loadComponent: ()=> import('./productos/productos.component').then((m)=> m.ProductosComponent),
-    pathMatch: 'full' 
+   
   },
   {
     path:'productos/:id',
     loadComponent:() =>import('./producto-detalle/producto-detalle.component').then((m) =>m.ProductoDetalleComponent)
   },
-  
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  }
 ];
