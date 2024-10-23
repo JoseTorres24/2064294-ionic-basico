f6eee761fb5e7c714a9051b41b7ea02da4ec4eaf
JoseTorres24 <luis.lopeztr@uanl.edu.mx>
2024-10-22 22:49:48 -0600

    Practica 11 Formularios

--- miApp-2064294/src/app/app.routes.ts
+++ miApp-2064294/src/app/app.routes.ts
@@ -14,11 +14,14 @@ export const routes: Routes = [
     path: 'productoDetalle/:id',  // ME EQUIVOQUE AQUIII TENIA TODO MAL REALIZADO ESTA PARTE AHHHHHH
     loadComponent: () => import('./producto-detalle/producto-detalle.component').then((m) => m.ProductoDetalleComponent),
   },
+  {
+    path: 'listaAlumnos',// Para poder poner el formulario en la direccion dirigirlo desdde el homePage
+    loadComponent: () => import('./lista-alumnos/lista-alumnos.component').then((m) => m.ListaAlumnosComponent),
+  },
   {// Por eso no me cargaba nadota:/
     path: '',
-    redirectTo: '/home',
+    redirectTo: 'home', 
     pathMatch: 'full', //<--- Esto era la parte mala tenia realmente mal manejado el estado inicio al momento de iniciar ionic serve  y que ese muestre el proyecto desde la pagina de home
   }
 ];
-
 
--- miApp-2064294/src/app/lista-alumnos/lista-alumnos.component.ts
+++ miApp-2064294/src/app/lista-alumnos/lista-alumnos.component.ts
@@ -1,59 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { IonList,IonItem,IonLabel,IonItemSliding,IonItemOptions,IonItemOption} from '@ionic/angular/standalone';
+import { IonList, IonItem, IonLabel, IonCheckbox, IonButton, IonInput, IonContent, IonHeader, IonToolbar, IonTitle, ActionSheetController } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';  // Importar FormsModule para usar NgModel
 import { CommonModule } from '@angular/common';
-import { ActionSheetController } from '@ionic/angular/standalone';
+import { alumno } from 'src/Intefaces/alumno.model';
+import { NgIf, NgFor } from '@angular/common';  // Importar NgIf y NgFor para las directivas estructurales
+
 @Component({
   selector: 'app-lista-alumnos',
   templateUrl: './lista-alumnos.component.html',
   styleUrls: ['./lista-alumnos.component.scss'],
-  standalone:true,
-  imports:[IonItem,IonList,CommonModule,IonLabel,IonItemSliding,IonItemOptions,IonItemOption]
+  standalone: true,
+  imports: [FormsModule,IonList, IonItem, IonLabel, IonCheckbox, IonButton, IonInput, IonContent, IonHeader, IonToolbar, IonTitle,CommonModule, NgIf,NgFor]
 })
-export class ListaAlumnosComponent  implements OnInit {
+export class ListaAlumnosComponent implements OnInit {
 
+  alumno: alumno = {
+    nombre: '',
+    presente: false
+  };
 
-   alumnos:string[] = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"];
+  alumnos: alumno[] = [];
 
-  constructor(private actionSheetController:ActionSheetController) { }
+  constructor(private actionSheetController: ActionSheetController) {}
+
   ngOnInit() {}
-    async agregarAFavoritos(alumno: string) {
-      console.log(`${alumno} agregado a favoritos`);
-    // Aquí puedes agregar la lógica para marcar al alumno como favorito
-    }
 
-    async eliminarAlumno(alumno: string) {
-    const actionSheet = await this.actionSheetController.create({
-      header: 'Confirmar Eliminación',
-      buttons: [
-        {
-          text: 'Eliminar',
-          role: 'destructive',
-          icon: 'trash',
-          handler: () => {
-            console.log(`${alumno} eliminado`);
-            this.alumnos = this.alumnos.filter(a => a !== alumno);
-          }
-        },
-        {
-          text: 'Cancelar',
-          role: 'cancel',
-          icon: 'close',
-          handler: () => {
-            console.log('Eliminación cancelada');
-          }
-        }
-      ]
-    });
-    await actionSheet.present();
+  agregarAlumno(): void {
+    if (this.alumno.nombre.trim() !== '') { // Verifica que el nombre no esté vacío
+      this.alumnos.push({ ...this.alumno });  // Agregar clon del objeto alumno para evitar referencias
+      this.alumno = {                          // Resetear el formulario
+        nombre: '',
+        presente: false
+      };
+    } else {
+      console.error('El nombre del alumno no puede estar vacío.');
     }
+  }
 }
-  
-  
-   
-  
-
-   
-
-   
-
 
